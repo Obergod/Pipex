33584e8a1e54218760566bcb8d1c5379bd057b4b
obergod <mael-loup@hotmail.fr>
2025-02-20 00:54:19 +0100

    babby steps on pipex

--- src/pipex.c
+++ src/pipex.c
@@ -23,16 +23,46 @@ void	child_process(int f1, )
 
 }
 
-//Get command count !
-void	pipex(int f1, int f2)
+t_cmd	*get_cmd(int ac, char **av)
 {
-	int	end[2];
-	pid_t	pid;
+	t_cmd	*cmd;
 
-	pipe(end);
-	pid = fork();
-	if (pid < 0)
-		return (perror("OMG NO\n"));
+	cmd = malloc(sizeof(t_cmd));
+	cmd->wc_cmd = ac - 3;
+	//parse to get all cmd (strlen for malloc ?)
+	//get_all_file etc...
+}
+
+void	pipex(int f1, int f2, int nb_cmd)
+{
+	int	*pipes[2];
+	pid_t	*pid;
+	int		i;
+
+	i = -1;
+	pid = malloc(sizeof(pid_t) * nb_cmd);
+	if (!pid)
+		return ;
+	pipes = malloc(sizeof(int *) * nb_cmd - 1);
+	if (!pipes)
+	{
+		free(pid);
+		return ;
+	}
+	while (++i < nb_cmd - 1)
+		pipe(pipes[i]);
+	i = -1;
+	while (++i < nb_cmd)
+	{
+		pid[i] = fork();
+		if (pid < 0)
+		{
+			//free pipes and pid
+			return (perror("OMG NO\n"));
+		}
+		//need to find a way to link in and out. dup2 ?
+	}
+	// need to use execv linked to the cmds
 	if (pid == 0)
 	{
 		printf("child process\n");
---
+++ src/pipex.h
@@ -0,0 +1,15 @@
+
+
+#ifndef PIPEX_H
+# define PIPEX_H
+
+#include <unistd.h>
+#include <stdlib.h>
+
+typedef struct	s_cmd
+{
+	t_list	cmd;
+	int		wc_cmd;
+}	t_cmd;
+
+#endif 
